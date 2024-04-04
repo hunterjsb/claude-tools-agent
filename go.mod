d31264c9c903b8799a427431fe18a828ab185107
hunter <hunterjsb@gmail.com>
2024-04-04 15:01:01 -0400

    get api key from os, load dotenv

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hunterjsb/super-claude
 
 go 1.22.2
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	cfg := newConfig(true)
+	cfg.load()
+}
+
+type Config struct {
+	requireDotEnv bool
+	claudeApiKey  string
+}
+
+func newConfig(requireDotEnv bool) *Config {
+	return &Config{requireDotEnv: requireDotEnv}
+}
+
+func (c *Config) load() {
+	err := godotenv.Load()
+	if err != nil {
+		if c.requireDotEnv {
+			log.Fatal("FATAL: Could not load .env")
+		} else {
+			fmt.Println("Could not load .env, continuing...")
+		}
+	}
+
+	apiKey := os.Getenv("CLAUDE_API_KEY")
+	if apiKey == "" {
+		log.Fatal("FATAL: could not find CLAUDE_API_KEY")
+	}
+
+	c.claudeApiKey = apiKey
+}
