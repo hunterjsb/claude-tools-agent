3a6059dc6b08847eb87bfdf3b7301dbdf28a41a7
hunter <hunterjsb@gmail.com>
2024-04-07 12:28:09 -0400

    tools.go ignore dirs that start with "__"

--- anthropic/tools.go
+++ anthropic/tools.go
@@ -52,56 +52,48 @@ func LoadToolFromJSONFile(filename string) (*Tool, error) {
 
 func LoadToolsFromDirectory(dir string) ([]Tool, error) {
 	toolJSONs := make([]Tool, 0)
-
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-
 		if path == "tools" {
 			return nil
 		}
-
 		if info.IsDir() {
 			toolName := filepath.Base(path)
+			if strings.HasPrefix(toolName, "__") {
+				return filepath.SkipDir
+			}
 			toolJSONPath := filepath.Join(path, toolName+".json")
 			toolGoPath := filepath.Join(path, toolName+".so")
-
 			// Load the tool JSON file
 			toolJSON, err := LoadToolFromJSONFile(toolJSONPath)
 			if err != nil {
 				return fmt.Errorf("failed to load tool JSON from file '%s': %v", toolJSONPath, err)
 			}
 			toolJSONs = append(toolJSONs, *toolJSON)
-
 			// Load the tool's Go plugin
 			plug, err := plugin.Open(toolGoPath)
 			if err != nil {
 				return fmt.Errorf("failed to load tool plugin from file '%s': %v", toolGoPath, err)
 			}
-
 			// Look up the UseTool function in the plugin
 			useToolFunc, err := plug.Lookup(strings.ToUpper(toolName))
 			if err != nil {
 				return fmt.Errorf("failed to find %s function in plugin '%s': %v", toolName, toolGoPath, err)
 			}
-
 			// Assert that the UseTool function has the correct type
 			useTool, ok := useToolFunc.(func(map[string]any) Content)
 			if !ok {
 				return fmt.Errorf("%s function in plugin '%s' has incorrect type", toolName, toolGoPath)
 			}
-
 			// Add the tool to the Tools map
 			ToolMap[toolName] = useTool
 		}
-
 		return nil
 	})
-
 	if err != nil {
 		return nil, fmt.Errorf("failed to walk directory '%s': %v", dir, err)
 	}
-
 	return toolJSONs, nil
 }
