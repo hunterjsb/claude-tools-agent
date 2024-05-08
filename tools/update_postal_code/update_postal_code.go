944f0be2638d80aaae06e22160e9ac0ab598ab87
hunter <hunterjsb@gmail.com>
2024-05-08 17:48:03 -0400

    removed unsupported tools

--- tools/update_postal_code/update_postal_code.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/hunterjsb/super-claude/anthropic"
-
-func UPDATE_POSTAL_CODE(params map[string]any) anthropic.Content {
-	return anthropic.Content{Type: anthropic.ToolResult, Content: "NOT IMPLEMENTED"}
-}
